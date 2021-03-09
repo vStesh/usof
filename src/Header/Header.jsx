504fb0abf2637f5ba21b83470f77d62e208c74fd
vstesh <stesh@ukr.net>
2021-03-09 09:37:14 +0200

    login

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -3,12 +3,48 @@ import style from './Header.module.css';
 import SearchTitle from './SearchTitle';
 import Buttons from './Buttons';
 
-let Header = () => {
+let Header = (props) => {
+
+    const clientId = 19740;
+    const key = '4bo*OgdhgS9algrG4b2fPg((';
+    const myUrl = 'https://vstesh.github.io/usof/';
+
+    // const auth = (data) => {
+
+    //     window.SecurityPolicyViolationEvent.autthenticate({
+    //         success: (data) => { 
+    //             tokens = data; 
+    //             console.log(tokens);
+    //             setToken(tokens.accessToken);
+    //             props.getUsersToken(tokens.accessToken);
+    //             setLoggedIn(true);
+    //         },
+    //         error: (data) => {  
+    //             alert('Я не получил доступ :('); 
+    //         },
+    //     });
+    // }
+
+    const init = () => {
+        window.SE.init({
+            clientId: 19740,
+            key: '4bo*OgdhgS9algrG4b2fPg((',
+            channelUrl: 'http://vstesh.github.io/usof/', 
+            complete: (data) => { console.log(data) } 
+        });
+        window.SE.authenticate({
+            success: function(data) { alert('Я получил доступ!'); }, // Приложение авторизовало пользователя
+            error: function(data) {  alert('Я не получил доступ :('); }, // Приложение не авторизовало пользователя
+        });
+    }
+init();
+
     return (
         <header className="header">
             <div className="main-container">
                 <div className={style.row}>
                     <div className={style.logo}>StackOwerflow</div>
+                
                     <SearchTitle />
                     <Buttons />
                 </div>
